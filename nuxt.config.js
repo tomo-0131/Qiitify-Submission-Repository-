232fa562c096d14878a14ff3af48328fb4a735f0
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-02-27 17:23:48 +0900

    add: aos導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios.js'
+    '~/plugins/axios.js',
+    { src: '~/plugins/aos', ssr: false }
   ],
 
   env: {
