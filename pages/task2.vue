b735252c0580571c5a385707a0ecb8b9edbf9a1c
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-03-02 20:57:06 +0900

    add:削除ボタン等の追加

--- pages/task2.vue
+++ pages/task2.vue
@@ -6,6 +6,7 @@
         <div class="task2__list">
           <ul>
             <li v-for="item in itemAll" :key="item.id" v-cloak>
+              記事ID：{{ item.id }}<br>
               <a :href="item.url" target="_blank">
               タイトル：{{ item.title }}
               </a><br>
@@ -13,12 +14,10 @@
               <h5>本文：{{item.body.slice(0, 150)}}……</h5>
               <div class="buttons">
                 <v-btn dark depressed color="primary">編集</v-btn>
-                <v-btn dark depressed color="error">削除</v-btn>
+                <v-btn v-on:click="deleteItems" dark depressed color="error">削除</v-btn>
               </div>
-            <v-divider />
-            <br>
-            </li>
-            <br>
+            <v-divider /><br>
+            </li><br>
           </ul>
         </div>
       </v-card>
@@ -33,6 +32,7 @@ export default {
   data () {
     return {
       itemAll: null,
+      items: '',
       message: ''
     }
   },
@@ -41,10 +41,24 @@ export default {
       axios.get('https://qiita.com/api/v2/users/tomoyuki_kt/items', { headers: { Authorization: 'Bearer 5e64526bf290713125443ba2a6d69bbb80073fa7' } })
         .then((response) => {
           this.itemAll = response.data
-          console.log(this.itemAll)
         })
         .catch((error) => {
-          this.message = error
+          this.message = 'エラー' + error
+        })
+    },
+    deleteItems () {
+      axios.delete('https://qiita.com/api/v2/items/db114ca264cb110b2cb0', { headers: { Authorization: 'Bearer 5e64526bf290713125443ba2a6d69bbb80073fa7' } })
+        .then((response) => {
+          const result = confirm('削除しますか')
+          if (result) {
+            alert('記事を削除しました！')
+            location.reload()
+          } else {
+            alert('削除を中断しました')
+          }
+        })
+        .catch((error) => {
+          this.message = 'エラー！' + error
         })
     }
   },
