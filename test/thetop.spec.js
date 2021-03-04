2545b2b0076e57575fd038d44cab8ad715a39692
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-03-04 19:01:51 +0900

    add: TheTopコンポーネントのjest作成

--- test/thetop.spec.js
+++ test/thetop.spec.js
@@ -2,13 +2,22 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import TheTop from '@/components/TheTop.vue'
 
-describe('components/TheTop', () => {
-  test('TheTopコンポーネントが存在することを確認', () => {
-    const wrapper = mount(TheTop)
-    Vuetify
-    expect(wrapper.exists()).toBeTruthy()
+const localVue = createLocalVue()
+
+describe('components/TheTop.vue', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(TheTop, {
+      localVue
+    })
+  })
+
+  describe('template', () => {
+    test('ユーザID入力input欄が存在することを確認', () => {
+      expect(wrapper.contains(v-text-field)).toBe(true)
+    })
   })
 })
