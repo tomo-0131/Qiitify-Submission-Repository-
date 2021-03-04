2a94d658a736d41fde8239eeaa0763294d72eb5b
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-03-04 14:35:57 +0900

    add: axios.js作成

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@
 export default function ({ $axios }) {
   $axios.onRequest((config) => {
     if (process.env.QIITA_TOKEN) {
-      config.headers.common["Authorization"] = process.env.QIITA_TOKEN
+      config.headers.common['Authorization'] = `Bearer ${process.env.QIITA_TOKEN}`
     }
     return config
   })
