2f67b9875099bd697d900a3d84168aa5c3591fd3
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-03-05 13:41:20 +0900

    add: jest作成

--- test/thetest.spec.js
+++ test/thetest.spec.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-Vue.use(Vuetify)
-
 import { mount, createLocalVue } from '@vue/test-utils'
-import TheTop from '@/components/TheTop.vue'
-
+import TheTest from '@/components/TheTest.vue'
+Vue.use(Vuetify)
 const localVue = createLocalVue()
 
 describe('components/TheTop.vue', () => {
   let wrapper
   beforeEach(() => {
-    wrapper = mount(TheTop, {
-      localVue
+    wrapper = mount(TheTest, {
+      localVue,
     })
   })
-
   describe('template', () => {
     test('ユーザID入力input欄が存在することを確認', () => {
-      expect(wrapper.contains(v-text-field)).toBe(true)
+      const inputForm = wrapper.find('#user-id-field')
+      expect(inputForm.exists()).toBe(true)
     })
   })
 })
